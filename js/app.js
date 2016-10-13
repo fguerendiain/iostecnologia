bae9b3494cf6e51dbf550c5248497ddfba2a4685
Franco Guerendiain <fguerendiain@gmail.com>
2016-10-13 09:35:49 -0300

    Ordenamiento de codigo y documentacion

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
-var appMainTitle = "IOS - Teconología";
+/*
+*
+* ESTRUCTURA DE VISTAS
+*
+*/
 
 var menuItemEmpresa = {
 	id:0,
 	viewUrl:"views/empresa.html",
 	domId:"menuEmpresa",
-	url:"empresa",
+	url:"/#/empresa",
 	title:"Empresa"
 };
 
@@ -12,31 +16,52 @@ var menuItemServicios = {
 	id:1,
 	viewUrl:"views/servicios.html",
 	domId:"menuServicios",
-	url:"servicios",
+	url:"/#/servicios",
 	title:"Servicios"
 };
 
-var menuItemTrabajos = {
-	id:2,
-	viewUrl:"views/trabajos.html",
-	domId:"menuTrabajos",
-	url:"trabajos",
-	title:"Trabajos"
-};
+// var menuItemTrabajos = {
+// 	id:2,
+// 	viewUrl:"views/trabajos.html",
+// 	domId:"menuTrabajos",
+// 	url:"trabajos",
+// 	title:"Trabajos"
+// };
 
 var menuItemContacto = {
 	id:3,
 	viewUrl:"views/contacto.html",
 	domId:"menuContacto",
-	url:"contacto",
+	url:"/#/contacto",
 	title:"Contacto"
 };
 
-var menuItems = [menuItemEmpresa, menuItemServicios, menuItemTrabajos, menuItemContacto];
+/*
+*
+*	ARRAY DE VISTAS
+*	VISTA DEFAULT
+*	URL TOMADA DEL NAVEGADOR RESTANDO LA /
+*
+*/
+
+var menuItems = [menuItemEmpresa, menuItemServicios, menuItemContacto];//, menuItemTrabajos];
 var defaultMenuItem = menuItemEmpresa;
+var currentUrl = window.location.pathname.substring(1);
+
+/*
+*
+*	DECLARACION DE FUNCIONES
+*
+*/
 
 
-
+/*
+*
+*summary
+*parameters
+*return
+*
+*/
 var loadContent = function(url){
 	$.ajax(url).done(function(data) {
 		$("#content").html(data);
@@ -44,6 +69,13 @@ var loadContent = function(url){
 	});
 }
 
+/*
+*
+*summary BUSCA LA VISTA SELECCIONADA POR EL USUARIO PARA MOSTRARLA EN PANTALLA
+*parameters RECIBE LA VISTA SELECCIONADA
+*return LLAMA A LA FUNCION QUE SETEA COMO ACTIVA LA VISTA
+*
+*/
 var updateMenu = function(menuItem){
 	for (var i in menuItems){
 		setMenuAsInactive(menuItems[i]);
@@ -52,14 +84,35 @@ var updateMenu = function(menuItem){
 	setMenuAsActive(menuItem);
 }
 
+/*
+*
+*summary SETEA COMO ACTIVA LA VISTA PASADA COMO PARAMETRO
+*parameters RECIBE LA VISTA  A ACTIVAR
+*return AGREGA LA CLASE "active" A LA VISTA
+*
+*/
 var setMenuAsActive = function(menuItem){
 	$("#"+menuItem.domId).addClass("active");
 }
 
+/*
+*
+*summary SETEA COMO INACTIVA LA VISTA PASADA COMO PARAMETRO
+*parameters RECIBE LA VISTA  A DESACTIVAR
+*return REMUEVE LA CLASE "active" A LA VISTA
+*
+*/
 var setMenuAsInactive = function(menuItem){
 	$("#"+menuItem.domId).removeClass("active");
 }
 
+/*
+*
+*summary CONTROLA EL PROCESO DE SELECCION DE VISTAS
+*parameters RECIBE EL MENU SELECCIONADO Y UN BOOLEANO QUE DETERMINA SI TIENE O NO ANTECESOR
+*return REALIZA EL PROCESO DE CAMBIO DE VISTA
+*
+*/
 var onClickMenu = function(menuItem, saveHistory){
 	updateMenu(menuItem);
 	updateBrowser(menuItem, saveHistory);
@@ -68,6 +121,17 @@ var onClickMenu = function(menuItem, saveHistory){
 	});
 }
 
+/*
+*
+*summary MUSTRA UN LOGO DE CARGA ENTRE EL CAMBIO DE VISTAS
+*parameters 
+*
+*	VER POR QUE SE PASA EL FLAG Y QUE ERA EL CB !!!!!!!!
+*
+*
+*return
+*
+*/
 var setLoadingStatus = function(flag, cb){
 	if (flag){
 		$("#content").fadeOut(250, function(){
@@ -82,6 +146,13 @@ var setLoadingStatus = function(flag, cb){
 	}
 }
 
+/*
+*
+*summary CAMBIA EL TITULO DE LA PESTAÑA DEL NAVEGADOR DE ACUERDO A LA VISTA
+*parameters RECIBE UNA VISTA Y UN BOOLEANO QUE DETERMINA LA PRECENCIA DE HISTORIAL
+*return CAMBIA EL NOMBRE Y DEPENDIENDO SI TIENE HITORIAL O NO, GUARDA EL ANTERIOR.
+*
+*/
 var updateBrowser = function(menuItem, saveHistory){
 	document.title = appMainTitle + " - " + menuItem.title;
 
@@ -92,6 +163,13 @@ var updateBrowser = function(menuItem, saveHistory){
 	}
 }
 
+/*
+*
+*summary AL LLAMAR ATRAS O ADENLANTE EL HISTORIAL LLAMA A ONCLICKMENU
+*parameters RECIBE EL EVENTO
+*return SI NO HAY HITORIAL SETEA LA VISTA DEFAULT SI HAY CARGA LA VISTA CORRESPONDIENTE
+*
+*/
 window.onpopstate = function(ev){
 	var menuItem = ev.state;
 	if (menuItem!= null){
@@ -101,7 +179,14 @@ window.onpopstate = function(ev){
 	}
 }
 
-var currentUrl = window.location.pathname.substring(1);
+
+/*
+*
+*summary
+*parameters
+*return
+*
+*/
 
 var found = false;
 for (var i in menuItems){
@@ -112,10 +197,13 @@ for (var i in menuItems){
 	}
 }
 
+
+/*
+*
+*	INSRTRUCCIONES A EJECUTAR
+*
+*/
+
 if (!found){
 	onClickMenu(defaultMenuItem, false);
 }
-
-$(".btn-block").click(function(){
-        $(".collapse").collapse('toggle');
-    });
