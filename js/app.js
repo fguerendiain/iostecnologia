f4eff2b94019de1036ea1a5ea34ef4b8ae8eb7ff
Franco Guerendiain <fguerendiain@gmail.com>
2016-10-14 11:57:27 -0300

    trabajo terminado

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
-/*
-*
-* ESTRUCTURA DE VISTAS
-*
-*/
+var appMainTitle = "IOS - Teconología";
 
+//ESTRUCTURAS DE VISTAS
 var menuItemEmpresa = {
 	id:0,
 	viewUrl:"views/empresa.html",
@@ -36,32 +33,11 @@ var menuItemContacto = {
 	title:"Contacto"
 };
 
-/*
-*
-*	ARRAY DE VISTAS
-*	VISTA DEFAULT
-*	URL TOMADA DEL NAVEGADOR RESTANDO LA /
-*
-*/
-
 var menuItems = [menuItemEmpresa, menuItemServicios, menuItemContacto];//, menuItemTrabajos];
 var defaultMenuItem = menuItemEmpresa;
-var currentUrl = window.location.pathname.substring(1);
-
-/*
-*
-*	DECLARACION DE FUNCIONES
-*
-*/
 
 
-/*
-*
-*summary
-*parameters
-*return
-*
-*/
+
 var loadContent = function(url){
 	$.ajax(url).done(function(data) {
 		$("#content").html(data);
@@ -69,13 +45,6 @@ var loadContent = function(url){
 	});
 }
 
-/*
-*
-*summary BUSCA LA VISTA SELECCIONADA POR EL USUARIO PARA MOSTRARLA EN PANTALLA
-*parameters RECIBE LA VISTA SELECCIONADA
-*return LLAMA A LA FUNCION QUE SETEA COMO ACTIVA LA VISTA
-*
-*/
 var updateMenu = function(menuItem){
 	for (var i in menuItems){
 		setMenuAsInactive(menuItems[i]);
@@ -84,35 +53,14 @@ var updateMenu = function(menuItem){
 	setMenuAsActive(menuItem);
 }
 
-/*
-*
-*summary SETEA COMO ACTIVA LA VISTA PASADA COMO PARAMETRO
-*parameters RECIBE LA VISTA  A ACTIVAR
-*return AGREGA LA CLASE "active" A LA VISTA
-*
-*/
 var setMenuAsActive = function(menuItem){
 	$("#"+menuItem.domId).addClass("active");
 }
 
-/*
-*
-*summary SETEA COMO INACTIVA LA VISTA PASADA COMO PARAMETRO
-*parameters RECIBE LA VISTA  A DESACTIVAR
-*return REMUEVE LA CLASE "active" A LA VISTA
-*
-*/
 var setMenuAsInactive = function(menuItem){
 	$("#"+menuItem.domId).removeClass("active");
 }
 
-/*
-*
-*summary CONTROLA EL PROCESO DE SELECCION DE VISTAS
-*parameters RECIBE EL MENU SELECCIONADO Y UN BOOLEANO QUE DETERMINA SI TIENE O NO ANTECESOR
-*return REALIZA EL PROCESO DE CAMBIO DE VISTA
-*
-*/
 var onClickMenu = function(menuItem, saveHistory){
 	updateMenu(menuItem);
 	updateBrowser(menuItem, saveHistory);
@@ -121,17 +69,6 @@ var onClickMenu = function(menuItem, saveHistory){
 	});
 }
 
-/*
-*
-*summary MUSTRA UN LOGO DE CARGA ENTRE EL CAMBIO DE VISTAS
-*parameters 
-*
-*	VER POR QUE SE PASA EL FLAG Y QUE ERA EL CB !!!!!!!!
-*
-*
-*return
-*
-*/
 var setLoadingStatus = function(flag, cb){
 	if (flag){
 		$("#content").fadeOut(250, function(){
@@ -146,13 +83,6 @@ var setLoadingStatus = function(flag, cb){
 	}
 }
 
-/*
-*
-*summary CAMBIA EL TITULO DE LA PESTAÑA DEL NAVEGADOR DE ACUERDO A LA VISTA
-*parameters RECIBE UNA VISTA Y UN BOOLEANO QUE DETERMINA LA PRECENCIA DE HISTORIAL
-*return CAMBIA EL NOMBRE Y DEPENDIENDO SI TIENE HITORIAL O NO, GUARDA EL ANTERIOR.
-*
-*/
 var updateBrowser = function(menuItem, saveHistory){
 	document.title = appMainTitle + " - " + menuItem.title;
 
@@ -163,13 +93,6 @@ var updateBrowser = function(menuItem, saveHistory){
 	}
 }
 
-/*
-*
-*summary AL LLAMAR ATRAS O ADENLANTE EL HISTORIAL LLAMA A ONCLICKMENU
-*parameters RECIBE EL EVENTO
-*return SI NO HAY HITORIAL SETEA LA VISTA DEFAULT SI HAY CARGA LA VISTA CORRESPONDIENTE
-*
-*/
 window.onpopstate = function(ev){
 	var menuItem = ev.state;
 	if (menuItem!= null){
@@ -179,14 +102,7 @@ window.onpopstate = function(ev){
 	}
 }
 
-
-/*
-*
-*summary
-*parameters
-*return
-*
-*/
+var currentUrl = window.location.pathname.substring(1);
 
 var found = false;
 for (var i in menuItems){
@@ -197,13 +113,10 @@ for (var i in menuItems){
 	}
 }
 
-
-/*
-*
-*	INSRTRUCCIONES A EJECUTAR
-*
-*/
-
 if (!found){
 	onClickMenu(defaultMenuItem, false);
 }
+
+$(".btn-block").click(function(){
+        $(".collapse").collapse('toggle');
+    });
